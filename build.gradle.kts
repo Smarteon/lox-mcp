763c9e22196ce8234adc630418bb99a0e03dd2c3
Tomáš Knotek <t.knotek22@gmail.com>
2026-01-31 15:16:48 +0100

    build: upgrade lox client to 0.7.0

--- build.gradle.kts
+++ build.gradle.kts
@@ -25,7 +25,7 @@ repositories {
 }
 
 dependencies {
-    implementation("cz.smarteon.loxone:loxone-client-kotlin-jvm:0.6.1")
+    implementation("cz.smarteon.loxone:loxone-client-kotlin-jvm:0.7.0")
     implementation("io.modelcontextprotocol:kotlin-sdk:0.8.1")
 
     implementation(libs.ktor.server.core)
